711542f85327da7576e0490d5a41bb3b43372e29
Gamal Elshamy <gamalelshamy74@gmail.com>
2025-09-02 01:13:18 +0300

    navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { AUTH_ROUTES } from './modules/auth/auth.routes';
+import { USER_ROUTES } from './modules/user/user.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadChildren: () => import('./modules/auth/auth.routes').then((m) => m.AUTH_ROUTES)
+    },
+    {
+        path: '',
+        loadChildren: () => import('./modules/user/user.routes').then((m) => m.USER_ROUTES)
+    },
+    {
+        path: '**',
+        loadComponent: () => import('./shared/components/not-found/not-found.component').then((c) => c.NotFoundComponent)
+    }
+];
---
+++ src/app/modules/user/user.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from "@angular/router";
+import { UserLayoutComponent } from "../../layouts/user-layout/user-layout.component";
+import { PRODUCTS_ROUTES } from "./products/products.routes";
+
+
+export const USER_ROUTES: Routes = [
+    {
+        path: '', component: UserLayoutComponent,
+        children: PRODUCTS_ROUTES
+    }
+]
