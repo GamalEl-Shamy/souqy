44cd36de66af3312f9dedc33758f99c85169abd6
Gamal Elshamy <gamalelshamy74@gmail.com>
2025-09-02 19:49:33 +0300

    added products from api

--- src/app/modules/user/products/pages/products/products.component.ts
+++ src/app/modules/user/products/pages/products/products.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
+import { Product } from '../../models/product';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  products: Product[] = []
+  private readonly productsService = inject(ProductsService)
+
+  ngOnInit(): void {
+    this.getAllProducts()
+  }
+
+  getAllProducts() {
+    this.productsService.getAllProducts().subscribe({
+      next: (res) => {
+        this.products = res.data
+      }
+    })
+  }
+
 
 }
