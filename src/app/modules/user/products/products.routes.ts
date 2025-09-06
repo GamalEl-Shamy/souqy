5103f22424ba482d6957082d788bdddfb3eb5678
Gamal Elshamy <gamalelshamy74@gmail.com>
2025-09-06 04:00:11 +0300

    added slug to the url to the products-details

--- src/app/modules/user/products/products.routes.ts
+++ src/app/modules/user/products/products.routes.ts
@@ -3,5 +3,5 @@ import { Routes } from "@angular/router";
 
 export const PRODUCTS_ROUTES: Routes = [
     { path: 'products', loadComponent: () => import('./pages/products/products.component').then((c) => c.ProductsComponent), title: "Products" },
-    { path: 'product-details/:id', loadComponent: () => import('./pages/product-details/product-details.component').then((c) => c.ProductDetailsComponent), title: "Product Details" }
+    { path: 'product-details/:slug/:id', loadComponent: () => import('./pages/product-details/product-details.component').then((c) => c.ProductDetailsComponent), title: "Product Details" }
 ]
