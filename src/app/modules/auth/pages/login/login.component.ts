2408b6c539b0882fc436ec17298d5c8c723aaaa3
Gamal Elshamy <gamalelshamy74@gmail.com>
2025-09-18 23:21:07 +0300

    added login page with design and logic

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormErrorMessagesComponent } from "../../../../shared/components/form-error-messages/form-error-messages.component";
+import { FormGroup, FormControl, Validators, AbstractControl, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { NgClass } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, FormErrorMessagesComponent, NgClass],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+
+  errorMsg: string = ''
+  isLoading: boolean = false
+  private readonly authService = inject(AuthService)
+  private readonly router = inject(Router)
+
+  authForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)]),
+  })
+
+
+
+  getValues() {
+    if (this.authForm.valid) {
+      this.isLoading = true
+      this.authService.login(this.authForm.value).subscribe({
+        next: (res) => {
+          console.log(res);
+          if (res.message == 'success') {
+            this.router.navigate(['/products'])
+            this.isLoading = false
+          }
+        },
+        error: (err) => {
+          console.log(err);
+          this.errorMsg = err.error.message
+          console.log(this.errorMsg);
+          this.isLoading = false
+
+        }
+      })
+    } else {
+      this.authForm.markAllAsTouched()
+    }
+  }
 
 }
