136fcf69d355b5c97f141c4120ba9365ff550760
Gamal Elshamy <gamalelshamy74@gmail.com>
2025-09-30 03:49:17 +0300

    update app routes server

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'product-details/:slug/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'address/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
